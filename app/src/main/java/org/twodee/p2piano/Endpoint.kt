53f0106bc22b71125143fa5dfea142fdce361092
Chris Johnson <crjjrc@gmail.com>
2019-05-14 11:08:58 +1200

    Wipe solutions for TODO.

--- app/src/main/java/org/twodee/p2piano/Endpoint.kt
+++ app/src/main/java/org/twodee/p2piano/Endpoint.kt
@@ -1,11 +1,4 @@
 package org.twodee.p2piano
 
-import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo
-
 // Exercise
-class Endpoint(val id: String, private val info: DiscoveredEndpointInfo) {
-  val name
-    get() = info.endpointName
-  override fun toString() = name
-}
 
--- app/src/main/java/org/twodee/p2piano/HostActivity.kt
+++ app/src/main/java/org/twodee/p2piano/HostActivity.kt
@@ -3,47 +3,12 @@ package org.twodee.p2piano
 import android.Manifest
 import android.os.Bundle
 import android.widget.Toast
-import com.google.android.gms.nearby.Nearby
-import com.google.android.gms.nearby.connection.*
 
 class HostActivity : KeyboardActivity() {
 
   // Exercise
-  private val payloadListener = object : PayloadCallback() {
-    override fun onPayloadReceived(id: String, payload: Payload) {
-      payload.asBytes()?.let { bytes ->
-        sendMidiMessage(bytes)
-      }
-    }
-
-    override fun onPayloadTransferUpdate(id: String, update: PayloadTransferUpdate) {}
-  }
 
   // Exercise
-  private val connectionListener = object : ConnectionLifecycleCallback() {
-    override fun onConnectionInitiated(id: String, info: ConnectionInfo) {
-      Toast.makeText(this@HostActivity, "Connecting to ${info.endpointName}.", Toast.LENGTH_SHORT).show()
-      Nearby.getConnectionsClient(this@HostActivity).acceptConnection(id, payloadListener)
-    }
-
-    override fun onConnectionResult(endpoint: String, result: ConnectionResolution) {
-      when (result.status.statusCode) {
-        ConnectionsStatusCodes.STATUS_OK -> {
-          Toast.makeText(this@HostActivity, "Connected", Toast.LENGTH_SHORT).show()
-        }
-        ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {
-          Toast.makeText(this@HostActivity, "Rejected", Toast.LENGTH_SHORT).show()
-        }
-        ConnectionsStatusCodes.STATUS_ERROR -> {
-          Toast.makeText(this@HostActivity, "Error", Toast.LENGTH_SHORT).show()
-        }
-      }
-    }
-
-    override fun onDisconnected(endpoint: String) {
-      Toast.makeText(this@HostActivity, "Disconnected", Toast.LENGTH_SHORT).show()
-    }
-  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -58,12 +23,5 @@ class HostActivity : KeyboardActivity() {
 
   // Exercise
   private fun advertise() {
-    val options = AdvertisingOptions.Builder().setStrategy(Strategy.P2P_POINT_TO_POINT).build()
-    Nearby.getConnectionsClient(this).startAdvertising("Chris", "P2Piano", connectionListener, options)
-      .addOnSuccessListener {
-        Toast.makeText(this, "Advertising...", Toast.LENGTH_LONG).show()
-      }.addOnFailureListener {
-        Toast.makeText(this, "Failed to advertise...", Toast.LENGTH_LONG).show()
-      }
   }
 }
